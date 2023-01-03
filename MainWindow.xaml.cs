ffad991d7b9907d23e6e0d9c058a4180de1483fb
Natheen-S <96934524+Natheen-S@users.noreply.github.com>
2023-01-03 23:58:43 +0530

    .Address

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -28,6 +28,7 @@ namespace Employeee_Details
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             string first = "";
+            string address = txtbox_address.Text;
             string second = $"With great pleasure, I would like extend the following employment offer.\r\n \r\nPosition: {txtbox_jobtitle.Text}\r\nStart date: 10/12/2022\r\nSalary: 50,000 per month\r\n \r\nThis employment offer is contingent upon the successful \rcompletion of background check, drug screening, reference check. \rThis offer is not a contract of employment, and either party \rmay terminate employment at any time, with or without cause. \r\n \r\nSincerely, \r\n HR Manager.";
             first = $"Dear { txtbox_name.Text },";
 
@@ -35,6 +36,7 @@ namespace Employeee_Details
             if (txtbox_name.Text != "" && txtbox_qualification.Text != "" && txtbox_address.Text != "" && txtbox_jobtitle.Text != "") 
             {
                 offer ooffer = new offer();
+                ooffer.lbl_address.Content = address;
                 ooffer.lbl_one.Content= first ;
                 ooffer.lbl_two.Content= second ;
                 ooffer.Show();
